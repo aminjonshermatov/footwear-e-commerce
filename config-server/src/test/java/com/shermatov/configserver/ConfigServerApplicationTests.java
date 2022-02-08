82bd3e7a181588a0e13ac2360e1ceadecaf00dbd
aminjonshermatov <aminjonshermatov@gmail.com>
2022-02-09 01:03:49 +0300

    configured some base services

---
+++ config-server/src/test/java/com/shermatov/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shermatov.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ users-service/src/main/java/com/shermatov/usersservice/UsersServiceApplication.java
@@ -0,0 +1,16 @@
+package com.shermatov.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+}
